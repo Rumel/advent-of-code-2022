2f4e6495e3056f5d9feafba16691cc044df7fdc0
Dalton Dick <daltondick2010@gmail.com>
2022-11-30 23:34:25 -0600

    Day 1 part A complete

--- src/problems/problem_01.clj
+++ src/problems/problem_01.clj
@@ -1,8 +1,33 @@
-(ns problems.problem-01)
+(ns problems.problem-01
+  "Day 1: Calorie Counting"
+  (:require [clojure.string :as str]))
 
-(defn answer-a [] "Not implemented yet")
-(defn answer-b [] "Not implemented yet")
+(defn get-calorie-totals [calories]
+  (map #(reduce + %) calories))
+
+(defn max-calories [calories]
+  (apply max calories))
+
+(defn convert-to-vec [s]
+  (let [lines (str/split-lines s)]
+    (loop [lines lines
+           ret-arr []
+           cur-arr []]
+      (if (empty? lines)
+        (conj ret-arr cur-arr)
+        (if (empty? (first lines))
+          (recur (rest lines) (conj ret-arr cur-arr) [])
+          (recur (rest lines) ret-arr (conj cur-arr (read-string (first lines)))))))))
+
+(defn answer-a [file]
+  (-> file
+      slurp
+      convert-to-vec
+      get-calorie-totals
+      max-calories))
+
+(defn answer-b [file] "Not implemented yet")
 
 (defn answer []
-  (println "01: A:" (answer-a))
-  (println "01: B:" (answer-b)))
+  (println "01: A:" (answer-a "data/problem-01-input.txt"))
+  (println "01: B:" (answer-b "data/problem-01-a.txt")))
--- test/problems/problem_01_test.clj
+++ test/problems/problem_01_test.clj
@@ -1,9 +1,19 @@
 (ns problems.problem-01-test
   (:require [clojure.test :as test :refer [deftest is]]
-            [problems.problem-01 :refer [answer-a answer-b]]))
+            [problems.problem-01 :refer [answer-a answer-b get-calorie-totals max-calories convert-to-vec]]))
+
+(deftest get-calorie-totals
+  (is (= [6000 4000 11000 24000 10000]
+         (get-calorie-totals [[1000 2000 3000] [4000] [5000 6000] [7000 8000 9000] [10000]]))))
+
+(deftest max-calories
+  (is (= 24000 (max-calories [6000 4000 11000 24000 10000]))))
+
+(deftest convert-to-vec
+  (is (= [[1 2 3] [4 5]] (convert-to-vec "1\n2\n3\n\n4\n5\n"))))
 
 (deftest answer-a-test
-  (is (= (answer-a) "Not implemented yet")))
+  (is (= (answer-a "data/problem-01-a.txt") 24000)))
 
 (deftest answer-b-test
-  (is (= (answer-b) "Not implemented yet")))
+  (is (= (answer-b "data/problem-01-a.txt") "Not implemented yet")))
