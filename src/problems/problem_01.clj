8df637aafaff481afccb5892ce9821e90a997d8d
Dalton Dick <daltondick2010@gmail.com>
2022-11-30 23:48:15 -0600

    Day 1 part B complete

--- src/problems/problem_01.clj
+++ src/problems/problem_01.clj
@@ -2,13 +2,19 @@
   "Day 1: Calorie Counting"
   (:require [clojure.string :as str]))
 
-(defn get-calorie-totals [calories]
+(defn get-calorie-totals
+  "Take the elves calories and get the total per elf"
+  [calories]
   (map #(reduce + %) calories))
 
-(defn max-calories [calories]
+(defn max-calories
+  "Return the max calories"
+  [calories]
   (apply max calories))
 
-(defn convert-to-vec [s]
+(defn convert-to-vec
+  "Take a string and convert it to a vector of vector of numbers."
+  [s]
   (let [lines (str/split-lines s)]
     (loop [lines lines
            ret-arr []
@@ -19,15 +25,30 @@
           (recur (rest lines) (conj ret-arr cur-arr) [])
           (recur (rest lines) ret-arr (conj cur-arr (read-string (first lines)))))))))
 
-(defn answer-a [file]
+(defn top-three-calories
+  "Return a vector of the top 3 calorie counts"
+  [calorie-counts]
+  (take 3 (sort > calorie-counts)))
+
+(defn answer-a
+  "Read the elves' calories and return the max elf's calories"
+  [file]
   (-> file
       slurp
       convert-to-vec
       get-calorie-totals
       max-calories))
 
-(defn answer-b [file] "Not implemented yet")
+(defn answer-b
+  "Read the elves' calories and return the max elf's calories"
+  [file]
+  (-> file
+      slurp
+      convert-to-vec
+      get-calorie-totals
+      top-three-calories
+      (as-> calories (reduce + calories))))
 
 (defn answer []
   (println "01: A:" (answer-a "data/problem-01-input.txt"))
-  (println "01: B:" (answer-b "data/problem-01-a.txt")))
+  (println "01: B:" (answer-b "data/problem-01-input.txt")))
--- test/problems/problem_01_test.clj
+++ test/problems/problem_01_test.clj
@@ -1,19 +1,23 @@
 (ns problems.problem-01-test
   (:require [clojure.test :as test :refer [deftest is]]
-            [problems.problem-01 :refer [answer-a answer-b get-calorie-totals max-calories convert-to-vec]]))
+            [problems.problem-01 :refer [answer-a answer-b get-calorie-totals max-calories convert-to-vec top-three-calories]]))
 
-(deftest get-calorie-totals
+(deftest get-calorie-totals-test
   (is (= [6000 4000 11000 24000 10000]
          (get-calorie-totals [[1000 2000 3000] [4000] [5000 6000] [7000 8000 9000] [10000]]))))
 
-(deftest max-calories
+(deftest max-calories-test
   (is (= 24000 (max-calories [6000 4000 11000 24000 10000]))))
 
-(deftest convert-to-vec
+(deftest top-three-calories-test
+  (is (= [24000 11000 10000]
+         (top-three-calories [6000 4000 11000 24000 10000]))))
+
+(deftest convert-to-vec-test
   (is (= [[1 2 3] [4 5]] (convert-to-vec "1\n2\n3\n\n4\n5\n"))))
 
 (deftest answer-a-test
   (is (= (answer-a "data/problem-01-a.txt") 24000)))
 
 (deftest answer-b-test
-  (is (= (answer-b "data/problem-01-a.txt") "Not implemented yet")))
+  (is (= (answer-b "data/problem-01-a.txt") 45000)))
