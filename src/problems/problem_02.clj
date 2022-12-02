eeaaa266334e66eef8be3dd503faa3999a42fd5e
Dalton Dick <daltondick2010@gmail.com>
2022-12-01 23:41:38 -0600

    Day 2 part A complete

--- src/problems/problem_02.clj
+++ src/problems/problem_02.clj
@@ -1,8 +1,39 @@
-(ns problems.problem-02)
+(ns problems.problem-02
+  (:require [clojure.string :as str]))
 
-(defn answer-a [] "Not implemented yet")
-(defn answer-b [] "Not implemented yet")
+
+(defn read-file
+  [file]
+  (->> file
+       slurp
+       str/split-lines
+       (map #(str/split % #" "))))
+
+(def throw-score {"X" 1 "Y" 2 "Z" 3})
+
+(def hand-result
+  {"A" {"X" 3 "Y" 6 "Z" 0}
+   "B" {"X" 0 "Y" 3 "Z" 6}
+   "C" {"X" 6 "Y" 0 "Z" 3}})
+
+(defn get-hand-score
+  [[h1 h2]]
+  (+ (throw-score h2) (get-in hand-result [h1 h2])))
+
+(defn get-score
+  [hands]
+  (->> hands
+       (map get-hand-score)
+       (reduce + 0)))
+
+(defn answer-a
+  [file]
+  (-> file
+      read-file
+      get-score))
+
+(defn answer-b [file] "Not implemented yet")
 
 (defn answer []
-  (println "02: A:" (answer-a))
-  (println "02: B:" (answer-b)))
+  (println "02: A:" (answer-a "data/problem-02-input.txt"))
+  (println "02: B:" (answer-b "data/problem-02-input.txt")))
--- test/problems/problem_02_test.clj
+++ test/problems/problem_02_test.clj
@@ -1,9 +1,26 @@
 (ns problems.problem-02-test
   (:require [clojure.test :as test :refer [deftest is]]
-            [problems.problem-02 :refer [answer-a answer-b]]))
+            [problems.problem-02 :refer
+             [answer-a
+              answer-b
+              get-hand-score
+              get-score]]))
+
+(def hands
+  [["A" "Y"]
+   ["B" "X"]
+   ["C" "Z"]])
+
+(deftest get-hand-score-test
+  (is (= 8 (get-hand-score (hands 0))))
+  (is (= 1 (get-hand-score (hands 1))))
+  (is (= 6 (get-hand-score (hands 2)))))
+
+(deftest get-score-test
+  (is (= 15 (get-score hands))))
 
 (deftest answer-a-test
-  (is (= (answer-a) "Not implemented yet")))
+  (is (= (answer-a "data/problem-02-a.txt") 15)))
 
 (deftest answer-b-test
-  (is (= (answer-b) "Not implemented yet")))
+  (is (= (answer-b "data/problem-02-b.txt") "Not implemented yet")))
