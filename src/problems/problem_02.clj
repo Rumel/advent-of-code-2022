46358b465fe00d3d2f29d091b507c4f8259e8e09
Dalton Dick <daltondick2010@gmail.com>
2022-12-01 23:56:55 -0600

    Day 2 Part B

--- src/problems/problem_02.clj
+++ src/problems/problem_02.clj
@@ -1,7 +1,6 @@
 (ns problems.problem-02
   (:require [clojure.string :as str]))
 
-
 (defn read-file
   [file]
   (->> file
@@ -16,23 +15,46 @@
    "B" {"X" 0 "Y" 3 "Z" 6}
    "C" {"X" 6 "Y" 0 "Z" 3}})
 
-(defn get-hand-score
+;; X need to lose
+;; Y need to draw
+;; Z need to win
+(def needed-throw
+  {"A" {"X" "Z" "Y" "X" "Z" "Y"}
+   "B" {"X" "X" "Y" "Y" "Z" "Z"}
+   "C" {"X" "Y" "Y" "Z" "Z" "X"}})
+
+(defn get-hand-score-a
   [[h1 h2]]
   (+ (throw-score h2) (get-in hand-result [h1 h2])))
 
-(defn get-score
+(defn get-hand-score-b
+  [[h1 h2]]
+  (let [thrown (get-in needed-throw [h1 h2])]
+    (+ (throw-score thrown) (get-in hand-result [h1 thrown]))))
+
+(defn get-score-a
   [hands]
   (->> hands
-       (map get-hand-score)
+       (map get-hand-score-a)
+       (reduce + 0)))
+
+(defn get-score-b
+  [hands]
+  (->> hands
+       (map get-hand-score-b)
        (reduce + 0)))
 
 (defn answer-a
   [file]
   (-> file
       read-file
-      get-score))
+      get-score-a))
 
-(defn answer-b [file] "Not implemented yet")
+(defn answer-b
+  [file]
+  (-> file
+      read-file
+      get-score-b))
 
 (defn answer []
   (println "02: A:" (answer-a "data/problem-02-input.txt"))
--- test/problems/problem_02_test.clj
+++ test/problems/problem_02_test.clj
@@ -3,24 +3,34 @@
             [problems.problem-02 :refer
              [answer-a
               answer-b
-              get-hand-score
-              get-score]]))
+              get-hand-score-a
+              get-hand-score-b
+              get-score-a
+              get-score-b]]))
 
 (def hands
   [["A" "Y"]
    ["B" "X"]
    ["C" "Z"]])
 
-(deftest get-hand-score-test
-  (is (= 8 (get-hand-score (hands 0))))
-  (is (= 1 (get-hand-score (hands 1))))
-  (is (= 6 (get-hand-score (hands 2)))))
+(deftest get-hand-score-a-test
+  (is (= 8 (get-hand-score-a (hands 0))))
+  (is (= 1 (get-hand-score-a (hands 1))))
+  (is (= 6 (get-hand-score-a (hands 2)))))
 
-(deftest get-score-test
-  (is (= 15 (get-score hands))))
+(deftest get-hand-score-b-test
+  (is (= 4 (get-hand-score-b (hands 0))))
+  (is (= 1 (get-hand-score-b (hands 1))))
+  (is (= 7 (get-hand-score-b (hands 2)))))
+
+(deftest get-score-a-test
+  (is (= 15 (get-score-a hands))))
+
+(deftest get-score-b-test
+  (is (= 12 (get-score-b hands))))
 
 (deftest answer-a-test
   (is (= (answer-a "data/problem-02-a.txt") 15)))
 
 (deftest answer-b-test
-  (is (= (answer-b "data/problem-02-b.txt") "Not implemented yet")))
+  (is (= (answer-b "data/problem-02-a.txt") 12)))
