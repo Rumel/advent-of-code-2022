7a110e560a620e02ec1a1bdc4deec7c6f5af6c02
Dalton Dick <daltondick2010@gmail.com>
2022-12-03 08:43:23 -0600

    Day 3 Part A

--- src/problems/problem_03.clj
+++ src/problems/problem_03.clj
@@ -1,8 +1,33 @@
-(ns problems.problem-03)
+(ns problems.problem-03
+  (:require [clojure.string :as str]
+            [clojure.set :as set]))
 
-(defn answer-a [] "Not implemented yet")
-(defn answer-b [] "Not implemented yet")
+(defn lines [file]
+  (->> (slurp file)
+       (str/split-lines)
+       (map #(char-array %))))
+
+(defn duplicate-char [line]
+  (let [n (/ (count line) 2)
+        a (take n line)
+        b (drop n line)]
+    (first (set/intersection (set a) (set b)))))
+
+(defn char-value [char]
+  (let [value (int char)]
+    (if (> value 90)
+      (- value 96)
+      (- value 38))))
+
+(defn answer-a [file]
+  (->> file
+       lines
+       (map #(duplicate-char %))
+       (map #(char-value %))
+       (reduce + 0)))
+
+(defn answer-b [file] "Not implemented yet")
 
 (defn answer []
-  (println "03: A:" (answer-a))
-  (println "03: B:" (answer-b)))
+  (println "03: A:" (answer-a "data/problem-03-input.txt"))
+  (println "03: B:" (answer-b "data/problem-03-input.txt")))
--- test/problems/problem_03_test.clj
+++ test/problems/problem_03_test.clj
@@ -1,9 +1,27 @@
 (ns problems.problem-03-test
   (:require [clojure.test :as test :refer [deftest is]]
-            [problems.problem-03 :refer [answer-a answer-b]]))
+            [problems.problem-03 :refer
+             [answer-a
+              answer-b
+              char-value
+              duplicate-char]]))
+
+(deftest duplicate-char-test
+  (is (= \p (duplicate-char "vJrwpWtwJgWrhcsFMMfFFhFp")))
+  (is (= \L (duplicate-char "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL")))
+  (is (= \P (duplicate-char "PmmdzqPrVvPwwTWBwg")))
+  (is (= \v (duplicate-char "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn")))
+  (is (= \t (duplicate-char "ttgJtRGJQctTZtZT")))
+  (is (= \s (duplicate-char "CrZsJsPPZsGzwwsLwLmpwMDw"))))
+
+(deftest char-value-test
+  (is (= 1 (char-value \a)))
+  (is (= 26 (char-value \z)))
+  (is (= 27 (char-value \A)))
+  (is (= 52 (char-value \Z))))
 
 (deftest answer-a-test
-  (is (= (answer-a) "Not implemented yet")))
+  (is (=  157 (answer-a "data/problem-03-a.txt"))))
 
 (deftest answer-b-test
-  (is (= (answer-b) "Not implemented yet")))
+  (is (=  "Not implemented yet" (answer-b "data/problem-03-a.txt"))))
