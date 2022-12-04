ccc2feb1bda429ebc700e7311ce7c92ee5a26549
Dalton Dick <daltondick2010@gmail.com>
2022-12-04 14:11:08 -0600

    Day 4 Part A

--- src/problems/problem_04.clj
+++ src/problems/problem_04.clj
@@ -1,8 +1,30 @@
-(ns problems.problem-04)
+(ns problems.problem-04
+  (:require [clojure.string :as str]))
 
-(defn answer-a [] "Not implemented yet")
-(defn answer-b [] "Not implemented yet")
+(defn lines [file] (str/split-lines (slurp file)))
+
+(defn nums [[a b]]
+  [(read-string a) (read-string b)])
+
+(defn convert [line]
+  (let [split (str/split line #",")
+        num-split (map #(str/split % #"-") split)]
+    (map nums num-split)))
+
+(defn contained? [[[a b] [c d]]]
+  (or (and (>= a c) (<= b d))
+      (and (>= c a) (<= d b))))
+
+(defn answer-a [file]
+  (->> file
+       lines
+       (map #(convert %))
+       (map #(contained? %))
+       (filter true?)
+       count))
+
+(defn answer-b [file] "Not implemented yet")
 
 (defn answer []
-  (println "04: A:" (answer-a))
-  (println "04: B:" (answer-b)))
+  (println "04: A:" (answer-a "data/problem-04-input.txt"))
+  (println "04: B:" (answer-b "data/problem-04-input.txt")))
--- test/problems/problem_04_test.clj
+++ test/problems/problem_04_test.clj
@@ -1,9 +1,22 @@
 (ns problems.problem-04-test
   (:require [clojure.test :as test :refer [deftest is]]
-            [problems.problem-04 :refer [answer-a answer-b]]))
+            [problems.problem-04 :refer
+             [answer-a
+              answer-b
+              contained?
+              convert]]))
+
+(deftest contained?-test
+  (is (= false (contained? [[2 4] [6 8]])))
+  (is (= true (contained? [[6 6] [4 6]]))))
+
+(deftest convert-test
+  (is (= [[2 4] [6 8]] (convert "2-4,6-8")))
+  (is (= [[2 3] [4 5]] (convert "2-3,4-5")))
+  (is (= [[5 7] [7 9]] (convert "5-7,7-9"))))
 
 (deftest answer-a-test
-  (is (= (answer-a) "Not implemented yet")))
+  (is (= 2 (answer-a "data/problem-04-a.txt"))))
 
 (deftest answer-b-test
-  (is (= (answer-b) "Not implemented yet")))
+  (is (= "Not implemented yet" (answer-b "data/problem-04-a.txt"))))
