b8f6c62908c75667a9ad4cc6935387c173a91178
Dalton Dick <daltondick2010@gmail.com>
2022-12-06 09:13:29 -0600

    Day 6 Part B

--- src/problems/problem_06.clj
+++ src/problems/problem_06.clj
@@ -1,8 +1,34 @@
-(ns problems.problem-06)
+(ns problems.problem-06
+  (:require [common.helpers :refer [input]]
+            [clojure.string :as str]))
 
-(defn answer-a [] "Not implemented yet")
-(defn answer-b [] "Not implemented yet")
+(defn get-arr [arr value]
+  (if (= 3 (count arr))
+    (conj arr value)
+    (conj (vec (drop 1 arr)) value)))
+
+(defn packet-index [s]
+  (let [split (str/split s #"")
+        r (range 1 (inc (count split)))
+        combined (map vector split r)]
+    ((reduce (fn [{:keys [arr result] :as d} value]
+               (if (not= result nil)
+                 d
+                 (let [arr (get-arr arr (first value))]
+                   (if (= 4 (count (set arr)))
+                     (assoc d :result (second value))
+                     (assoc d :arr arr))))) {:arr (vec (map first (take 3 combined))) :result nil} (drop 3 combined)) :result)))
+
+(defn answer-a [file]
+  (->> file
+       input
+       first
+       packet-index))
+
+(answer-a "data/problem-06-a.txt")
+
+(defn answer-b [file] "Not implemented yet")
 
 (defn answer []
-  (println "06: A:" (answer-a))
-  (println "06: B:" (answer-b)))
+  (println "06: A:" (answer-a "data/problem-06-input.txt"))
+  (println "06: B:" (answer-b "data/problem-06-input.txt")))
--- test/problems/problem_06_test.clj
+++ test/problems/problem_06_test.clj
@@ -1,9 +1,24 @@
 (ns problems.problem-06-test
   (:require [clojure.test :as test :refer [deftest is]]
-            [problems.problem-06 :refer [answer-a answer-b]]))
+            [problems.problem-06 :refer
+             [answer-a
+              answer-b
+              get-arr
+              packet-index]]))
+
+(deftest get-arr-test
+  (is (= [1 2 3 4] (get-arr [1 2 3] 4)))
+  (is (= [2 3 4 5] (get-arr [1 2 3 4] 5))))
+
+(deftest packet-index-test
+  (is (= 7 (packet-index "mjqjpqmgbljsphdztnvjfqwrcgsmlb")))
+  (is (= 5 (packet-index "bvwbjplbgvbhsrlpgdmjqwftvncz")))
+  (is (= 6 (packet-index "nppdvjthqldpwncqszvftbrmjlhg")))
+  (is (= 10 (packet-index "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")))
+  (is (= 11 (packet-index "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"))))
 
 (deftest answer-a-test
-  (is (= (answer-a) "Not implemented yet")))
+  (is (=  7 (answer-a "data/problem-06-a.txt"))))
 
 (deftest answer-b-test
-  (is (= (answer-b) "Not implemented yet")))
+  (is (= "Not implemented yet" (answer-a "data/problem-06-a.txt"))))
