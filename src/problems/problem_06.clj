0e258042fde39a6c204804a2fc6c465f61fe225e
Dalton Dick <daltondick2010@gmail.com>
2022-12-06 09:20:46 -0600

    Day 6 Part B

--- src/problems/problem_06.clj
+++ src/problems/problem_06.clj
@@ -2,32 +2,34 @@
   (:require [common.helpers :refer [input]]
             [clojure.string :as str]))
 
-(defn get-arr [arr value]
-  (if (= 3 (count arr))
+(defn get-arr [arr value n]
+  (if (= (dec n) (count arr))
     (conj arr value)
     (conj (vec (drop 1 arr)) value)))
 
-(defn packet-index [s]
+(defn packet-index [n s]
   (let [split (str/split s #"")
         r (range 1 (inc (count split)))
         combined (map vector split r)]
     ((reduce (fn [{:keys [arr result] :as d} value]
                (if (not= result nil)
                  d
-                 (let [arr (get-arr arr (first value))]
-                   (if (= 4 (count (set arr)))
+                 (let [arr (get-arr arr (first value) n)]
+                   (if (= n (count (set arr)))
                      (assoc d :result (second value))
-                     (assoc d :arr arr))))) {:arr (vec (map first (take 3 combined))) :result nil} (drop 3 combined)) :result)))
+                     (assoc d :arr arr))))) {:arr (vec (map first (take (dec n) combined))) :result nil} (drop (dec n) combined)) :result)))
 
 (defn answer-a [file]
   (->> file
        input
        first
-       packet-index))
+       (packet-index 4)))
 
-(answer-a "data/problem-06-a.txt")
-
-(defn answer-b [file] "Not implemented yet")
+(defn answer-b [file]
+  (->> file
+       input
+       first
+       (packet-index 14)))
 
 (defn answer []
   (println "06: A:" (answer-a "data/problem-06-input.txt"))
--- test/problems/problem_06_test.clj
+++ test/problems/problem_06_test.clj
@@ -7,18 +7,23 @@
               packet-index]]))
 
 (deftest get-arr-test
-  (is (= [1 2 3 4] (get-arr [1 2 3] 4)))
-  (is (= [2 3 4 5] (get-arr [1 2 3 4] 5))))
+  (is (= [1 2 3 4] (get-arr [1 2 3] 4 4)))
+  (is (= [2 3 4 5] (get-arr [1 2 3 4] 5 4))))
 
 (deftest packet-index-test
-  (is (= 7 (packet-index "mjqjpqmgbljsphdztnvjfqwrcgsmlb")))
-  (is (= 5 (packet-index "bvwbjplbgvbhsrlpgdmjqwftvncz")))
-  (is (= 6 (packet-index "nppdvjthqldpwncqszvftbrmjlhg")))
-  (is (= 10 (packet-index "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")))
-  (is (= 11 (packet-index "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"))))
+  (is (= 7 (packet-index 4 "mjqjpqmgbljsphdztnvjfqwrcgsmlb")))
+  (is (= 5 (packet-index 4 "bvwbjplbgvbhsrlpgdmjqwftvncz")))
+  (is (= 6 (packet-index 4 "nppdvjthqldpwncqszvftbrmjlhg")))
+  (is (= 10 (packet-index 4 "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")))
+  (is (= 11 (packet-index 4 "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")))
+  (is (= 19 (packet-index 14 "mjqjpqmgbljsphdztnvjfqwrcgsmlb")))
+  (is (= 23 (packet-index 14 "bvwbjplbgvbhsrlpgdmjqwftvncz")))
+  (is (= 23 (packet-index 14 "nppdvjthqldpwncqszvftbrmjlhg")))
+  (is (= 29 (packet-index 14 "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")))
+  (is (= 26 (packet-index 14 "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"))))
 
 (deftest answer-a-test
   (is (=  7 (answer-a "data/problem-06-a.txt"))))
 
 (deftest answer-b-test
-  (is (= "Not implemented yet" (answer-a "data/problem-06-a.txt"))))
+  (is (= 19 (answer-b "data/problem-06-a.txt"))))
