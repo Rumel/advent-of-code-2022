2d5c9c6ccd44cac79148639ce84a765177e726a9
Dalton Dick <daltondick2010@gmail.com>
2022-12-07 11:06:39 -0600

    Day 7 Part A

--- src/problems/problem_07.clj
+++ src/problems/problem_07.clj
@@ -1,8 +1,86 @@
-(ns problems.problem-07)
+(ns problems.problem-07
+  (:require [clojure.pprint :as pprint]
+            [clojure.string :as str]
+            [common.helpers :refer [input]]))
 
-(defn answer-a [] "Not implemented yet")
-(defn answer-b [] "Not implemented yet")
+(def base-map {:cwd "" :dirs {}})
+
+
+(def cd-regex #"\$ cd (.+)")
+(defn matches-cd [line] (not (nil? (re-matches cd-regex line))))
+(defn cd-dir
+  "Change :cwd in the map to the directory requested"
+  [line d]
+  (let [cwd (d :cwd)
+        [_ dir] (re-matches cd-regex line)]
+    (cond
+      (= "/" dir) (assoc d :cwd "/")
+      (= ".." dir) (let [new-dir (->> (str/split cwd #"/")
+                                      drop-last
+                                      (str/join "/"))]
+                     (if (= new-dir "")
+                       (assoc d :cwd "/")
+                       (assoc d :cwd new-dir)))
+      :else (if (= cwd "/")
+              (assoc d :cwd (str cwd dir))
+              (assoc d :cwd (str cwd "/" dir))))))
+
+(defn get-directory-split [dir]
+  (let [split (str/split dir #"/")]
+    (if (empty? split)
+      [""]
+      split)))
+(def file-regex #"(\d+) (.*)")
+(defn matches-file [line] (not (nil? (re-matches file-regex line))))
+(defn add-file
+  "Add file to the directory and all super directories"
+  [line d]
+  (let [[_ size-str filename] (re-matches file-regex line)
+        size (read-string size-str)
+        cwd (d :cwd)
+        dir-split (get-directory-split cwd)]
+    (-> (reduce (fn [[cwd d] new-dir]
+                  (if (empty? new-dir)
+                    [cwd (update-in d [:dirs "/"] conj {:size size :file filename})]
+                    (let [updated-dir (str cwd "/" new-dir)]
+                      [updated-dir (update-in d [:dirs updated-dir] conj {:size size :file filename})])))
+                ["" d]
+                dir-split)
+        second)))
+
+(defn create-directory-structure
+  "Creates the directory structure with files"
+  [lines]
+  (reduce (fn [d line]
+            (cond
+              (matches-cd line) (cd-dir line d)
+              (matches-file line) (add-file line d)
+              :else d))
+          base-map lines))
+
+(defn directory-file-sizes
+  "Returns a vector of the directories with their sizes
+   Input looks like [\"/dir/lemur\" [{:file \"test\" :size 23} {:file \"test2\" :size 45}]]
+   Returns: [\"/dir/lemur\" 68]
+   "
+  [[dir files]]
+  (let [file-sizes (map #(% :size) files)
+        total (reduce + 0 file-sizes)]
+    [dir total]))
+
+(defn answer-a [file]
+  (-> file
+      input
+      create-directory-structure
+      (get :dirs)
+      vec
+      (->> (map directory-file-sizes)
+           (map #(second %))
+           (filter #(<= % 100000))
+           (reduce + 0))))
+
+(defn answer-b [file] "Not implemented yet")
 
 (defn answer []
-  (println "07: A:" (answer-a))
-  (println "07: B:" (answer-b)))
+  (println "07: A:" (answer-a "data/problem-07-input.txt"))
+  (println "07: B:" (answer-b "data/problem-07-input.txt")))
--- test/problems/problem_07_test.clj
+++ test/problems/problem_07_test.clj
@@ -1,9 +1,60 @@
 (ns problems.problem-07-test
   (:require [clojure.test :as test :refer [deftest is]]
-            [problems.problem-07 :refer [answer-a answer-b]]))
+            [problems.problem-07
+             :refer
+             [add-file
+              answer-a
+              answer-b
+              cd-dir
+              directory-file-sizes
+              get-directory-split
+              matches-cd
+              matches-file]]))
+
+
+(deftest matches-cd-test
+  (is (true? (matches-cd "$ cd /")))
+  (is (true? (matches-cd "$ cd ..")))
+  (is (true? (matches-cd "$ cd lemur")))
+  (is (false? (matches-cd "126880 fmftdzrp.fwt"))))
+
+(def starting-directory {:cwd "/tmp/lemur"})
+(deftest cd-dir-test
+  (is (= "/" ((cd-dir "$ cd /" {:cwd ""}) :cwd)))
+  (is (= "/tmp/lemur/baseball" ((cd-dir "$ cd baseball" starting-directory) :cwd)))
+  (is (= "/tmp" ((cd-dir "$ cd .." starting-directory) :cwd)))
+  (is (= "/a" ((cd-dir "$ cd a" {:cwd "/"}) :cwd)))
+  (is (= "/" ((cd-dir "$ cd .." {:cwd "/a"}) :cwd))))
+
+(deftest matches-file-test
+  (is (true? (matches-file "126880 fmftdzrp.fwt")))
+  (is (true? (matches-file "173625 hhfqgzfj.qvt")))
+  (is (false? (matches-file "$ cd /"))))
+
+(deftest get-directory-split-test
+  (is (= [""] (get-directory-split "/")))
+  (is (= ["" "a"] (get-directory-split "/a")))
+  (is (= ["" "a" "b"] (get-directory-split "/a/b"))))
+
+(def root-dir {:cwd "/" :dirs {}})
+(def a-dir {:cwd "/a" :dirs {}})
+(def b-dir {:cwd "/a/b" :dirs {}})
+(def file-arr [{:file "test.txt" :size 1}])
+(deftest add-file-test
+  (is (= {:cwd "/" :dirs {"/" file-arr}}
+         (add-file "1 test.txt" root-dir)))
+  (is (= {:cwd "/a" :dirs {"/" file-arr "/a" file-arr}}
+         (add-file "1 test.txt" a-dir)))
+  (is (= {:cwd "/a/b" :dirs {"/" file-arr "/a" file-arr "/a/b" file-arr}}
+         (add-file "1 test.txt" b-dir))))
+
+(deftest directory-file-sizes-test
+  (is (= ["/tmp/lemur" 43] (directory-file-sizes ["/tmp/lemur" [{:file "test" :size 23} {:file "test2" :size 20}]])))
+  (is (= ["/tmp/empty" 0] (directory-file-sizes ["/tmp/empty" []]))))
 
 (deftest answer-a-test
-  (is (= (answer-a) "Not implemented yet")))
+  (is (= 95437 (answer-a "data/problem-07-a.txt")))
+  (is (= 1644735 (answer-a "data/problem-07-input.txt"))))
 
 (deftest answer-b-test
-  (is (= (answer-b) "Not implemented yet")))
+  (is (= "Not implemented yet" (answer-b "data/problem-07-a.txt"))))
