208357363b4b7b06410a681cf884b888f32ce643
Dalton Dick <daltondick2010@gmail.com>
2022-12-11 12:50:27 -0600

    Day 11

--- src/problems/problem_11.clj
+++ src/problems/problem_11.clj
@@ -1,8 +1,132 @@
-(ns problems.problem-11)
+(ns problems.problem-11
+  (:require [common.helpers :refer [input]]
+            [clojure.string :as str]))
 
-(defn answer-a [] "Not implemented yet")
-(defn answer-b [] "Not implemented yet")
+(defn get-num [s]
+  (read-string s))
+
+(defn parse-monkey-index
+  [line]
+  (-> (re-matches #"Monkey (\d+):" line)
+      (get 1)
+      read-string))
+
+(defn parse-items
+  [line]
+  (-> line
+      (str/split #":")
+      (get 1)
+      (str/split #",")
+      (->> (map get-num))))
+
+(defn operation-func
+  [[x y z] divide-by-three]
+  (let [operator (case y
+                   "*" *
+                   "+" +
+                   :else (throw (Exception. "Not + or *")))]
+    (fn [old]
+      (let [result (operator
+                    (if (= x "old")
+                      old
+                      (get-num x))
+                    (if (= z "old")
+                      old
+                      (get-num z)))]
+        (if divide-by-three
+          (int (/ result 3))
+          result)))))
+
+(defn parse-operation
+  [line divide-by-three]
+  (-> line
+      (str/split #"=")
+      (get 1)
+      str/triml
+      (str/split #" ")
+      (operation-func divide-by-three)))
+
+(defn parse-last-num
+  [line]
+  (-> line
+      (str/split #" ")
+      last
+      read-string))
+
+(defn create-monkey [lines divide-by-three]
+  (let [lines (vec lines)
+        index (parse-monkey-index (lines 0))
+        items (parse-items (lines 1))
+        operation (parse-operation (lines 2) divide-by-three)
+        test-num (parse-last-num (lines 3))
+        true-monkey (parse-last-num (lines 4))
+        false-monkey (parse-last-num (lines 5))]
+    {:index index
+     :items items
+     :operation operation
+     :test-num test-num
+     :test-fn (fn [item]
+                (= 0 (mod item test-num)))
+     :true-monkey true-monkey
+     :false-monkey false-monkey
+     :inspections 0}))
+
+(defn parse-input [file divide-by-three]
+  (->> file
+       input
+       (filter (comp not empty?))
+       (partition 6)
+       (map #(create-monkey % divide-by-three))))
+
+(defn execute-monkey
+  [d i]
+  (let [monkey ((d :monkeys) i)
+        items (monkey :items)
+        transformed-items (map #(mod % (d :mod-num)) items)
+        transformed-items (map (monkey :operation) transformed-items)
+        d (assoc-in d [:monkeys (monkey :index) :items] '())]
+    (reduce
+     (fn [d item]
+       (let [throw-monkey ((monkey :test-fn) item)
+             monkey-index (if throw-monkey
+                            (monkey :true-monkey)
+                            (monkey :false-monkey))]
+         (-> d
+             (update-in [:monkeys monkey-index :items] conj item)
+             (update-in [:monkeys i :inspections] inc))))
+     d
+     transformed-items)))
+
+(defn execute-round
+  [d round-num]
+  ;; (println "Round: " round-num)
+  (let [result (reduce execute-monkey d (map :index (d :monkeys)))]
+    ;; (println (map (comp vec :items) (result :monkeys)))
+    ;; (println result)
+    result))
+
+(defn execute-rounds
+  [file rounds divide-by-three]
+  (let [monkeys (vec (-> file
+                         (parse-input divide-by-three)))
+        mod-num (apply * (map :test-num monkeys))
+        result (reduce execute-round {:monkeys monkeys :mod-num mod-num} (range 1 (inc rounds)))]
+    (map :inspections (result :monkeys))))
+
+(defn answer-a [file]
+  (->> (execute-rounds file 20 true)
+       sort
+       reverse
+       (take 2)
+       (apply *)))
+
+(defn answer-b [file]
+  (->> (execute-rounds file 10000 false)
+       sort
+       reverse
+       (take 2)
+       (apply *)))
 
 (defn answer []
-  (println "11: A:" (answer-a))
-  (println "11: B:" (answer-b)))
+  (println "11: A:" (answer-a "data/problem-11-input.txt"))
+  (println "11: B:" (answer-b "data/problem-11-input.txt")))
--- test/problems/problem_11_test.clj
+++ test/problems/problem_11_test.clj
@@ -1,9 +1,28 @@
 (ns problems.problem-11-test
   (:require [clojure.test :as test :refer [deftest is]]
-            [problems.problem-11 :refer [answer-a answer-b]]))
+            [problems.problem-11
+             :refer
+             [answer-a
+              answer-b
+              execute-rounds]]))
+
+(deftest execute-rounds-test
+  (is (= '(2 4 3 6) (execute-rounds "data/problem-11-a.txt" 1 false)))
+  (is (= '(99 97 8 103) (execute-rounds "data/problem-11-a.txt" 20 false)))
+  (is (= '(5204 4792 199 5192) (execute-rounds "data/problem-11-a.txt" 1000 false)))
+  (is (= '(10419 9577 392 10391) (execute-rounds "data/problem-11-a.txt" 2000 false)))
+  (is (= '(15638 14358 587 15593) (execute-rounds "data/problem-11-a.txt" 3000 false)))
+  (is (= '(20858 19138 780 20797) (execute-rounds "data/problem-11-a.txt" 4000 false)))
+  (is (= '(26075 23921 974 26000) (execute-rounds "data/problem-11-a.txt" 5000 false)))
+  (is (= '(31294 28702 1165 31204) (execute-rounds "data/problem-11-a.txt" 6000 false)))
+  (is (= '(36508 33488 1360 36400) (execute-rounds "data/problem-11-a.txt" 7000 false)))
+  (is (= '(41728 38268 1553 41606) (execute-rounds "data/problem-11-a.txt" 8000 false)))
+  (is (= '(46945 43051 1746 46807) (execute-rounds "data/problem-11-a.txt" 9000 false)))
+  (is (= '(52166 47830 1938 52013) (execute-rounds "data/problem-11-a.txt" 10000 false))))
 
 (deftest answer-a-test
-  (is (= (answer-a) "Not implemented yet")))
+  (is (= 10605 (answer-a "data/problem-11-a.txt")))
+  (is (= 54253 (answer-a "data/problem-11-input.txt"))))
 
 (deftest answer-b-test
-  (is (= (answer-b) "Not implemented yet")))
+  (is (= 2713310158 (answer-b "data/problem-11-a.txt"))))
