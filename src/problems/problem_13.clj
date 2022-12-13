9f0d4dc6fe2b75ab71ca4cae7266cc47cd444f60
Dalton Dick <daltondick2010@gmail.com>
2022-12-13 15:16:34 -0600

    Day 13

--- src/problems/problem_13.clj
+++ src/problems/problem_13.clj
@@ -1,8 +1,131 @@
-(ns problems.problem-13)
+(ns problems.problem-13
+  (:require [common.helpers :refer [input]]))
 
-(defn answer-a [] "Not implemented yet")
-(defn answer-b [] "Not implemented yet")
+(defn parsed-input
+  "Transforms the input to something like this:
+([0 ([1 1 3 1 1] [1 1 5 1 1])]
+ [1 ([[1] [2 3 4]] [[1] 4])]
+ [2 ([9] [[8 7 6]])]
+ [3 ([[4 4] 4 4] [[4 4] 4 4 4])]
+ [4 ([7 7 7 7] [7 7 7])]
+ [5 ([] [3])]
+ [6 ([[[]]] [[]])]
+ [7 ([1 [2 [3 [4 [5 6 7]]]] 8 9] [1 [2 [3 [4 [5 6 0]]]] 8 9])])
+"
+  [file]
+  (->> file
+       input
+       (map #(if (empty? %) nil %))
+       (filter #((comp not nil?) %))
+       (map read-string)))
+
+(defn is-long?
+  [l]
+  (= java.lang.Long (class l)))
+
+(defn is-list?
+  [l]
+  (= clojure.lang.PersistentVector (class l)))
+
+(defn correct-packets?
+  [a b]
+  (loop [af (first a)
+         ar (rest a)
+         bf (first b)
+         br (rest b)]
+    (cond
+      (and (nil? af)
+           (nil? bf))
+      :next
+
+      (nil? af)
+      true
+
+      (nil? bf)
+      false
+
+      (and (true? (is-long? af))
+           (true? (is-long? bf)))
+      (if (= af bf)
+        (recur (first ar) (rest ar) (first br) (rest br))
+        (if (< af bf)
+          true
+          false))
+
+      (and (true? (is-long? af))
+           (true? (is-list? bf)))
+      (let [result (correct-packets? [af] bf)]
+        (if (= result :next)
+          (recur (first ar) (rest ar) (first br) (rest br))
+          result))
+
+
+      (and (true? (is-list? af))
+           (true? (is-long? bf)))
+      (let [result (correct-packets? af [bf])]
+        (if (= result :next)
+          (recur (first ar) (rest ar) (first br) (rest br))
+          result))
+
+
+      (and (true? (is-list? af))
+           (true? (is-list? bf)))
+      (if (and (= af [])
+               (= bf []))
+        (recur (first ar) (rest ar) (first br) (rest br))
+        (let [result (correct-packets? af bf)]
+          (if (= result :next)
+            (recur (first ar) (rest ar) (first br) (rest br))
+            result))))))
+
+
+(defn indices-sum
+  [lines]
+  (reduce
+   (fn [sum line]
+     (let [index (inc (first line))
+           [packet-a packet-b] (second line)]
+       (if (correct-packets? packet-a packet-b)
+         (+ sum index)
+         sum)))
+   0
+   lines))
+
+(defn find-indices
+  [lines]
+  (let [indexed (map-indexed
+                 (fn [i line]
+                   [(inc i) line])
+                 lines)
+        finder (fn [x]
+                 (filter #(= x (second %)) indexed))
+        two-index (ffirst (finder [[2]]))
+        six-index (ffirst (finder [[6]]))]
+    (* two-index six-index)))
+
+(defn answer-a
+  [file]
+  (->> file
+       parsed-input
+       (partition 2)
+       (map-indexed #(identity [%1 %2]))
+       indices-sum))
+
+(defn sorter
+  [a b]
+  (let [result (correct-packets? a b)]
+    (if (= result :next)
+      true
+      result)))
+
+(defn answer-b
+  [file]
+  (->> file
+       parsed-input
+       (concat [[[2]]] [[[6]]])
+       (sort sorter)
+       (find-indices)))
 
 (defn answer []
-  (println "13: A:" (answer-a))
-  (println "13: B:" (answer-b)))
+  (println "13: A:" (answer-a "data/problem-13-input.txt"))
+  (println "13: B:" (answer-b "data/problem-13-input.txt")))
--- test/problems/problem_13_test.clj
+++ test/problems/problem_13_test.clj
@@ -1,9 +1,71 @@
 (ns problems.problem-13-test
   (:require [clojure.test :as test :refer [deftest is]]
-            [problems.problem-13 :refer [answer-a answer-b]]))
+            [problems.problem-13
+             :refer
+             [answer-a
+              answer-b
+              correct-packets?
+              sorter]]))
+
+(deftest correct-packets?-test
+  (is (= true (correct-packets?
+               [1 1 3 1 1]
+               [1 1 5 1 1])))
+  (is (= true (correct-packets?
+               [[1] [2 3 4]]
+               [[1] 4])))
+  (is (= false (correct-packets?
+                [9]
+                [[8 7 6]])))
+  (is (= true (correct-packets?
+               [[4 4] 4 4]
+               [[4 4] 4 4 4])))
+  (is (= false (correct-packets?
+                [7 7 7 7]
+                [7 7 7])))
+  (is (= true (correct-packets?
+               []
+               [3])))
+  (is (= false (correct-packets?
+                [[[]]]
+                [[]])))
+  (is (= false (correct-packets?
+                [1 [2 [3 [4 [5 6 7]]]] 8 9]
+                [1 [2 [3 [4 [5 6 0]]]] 8 9])))
+;; Copying from input file
+  (is (= false (correct-packets?
+                [[7],[[[5,3,3,4],[0],[1,7,2,4,2],4,[10,8,3,1]],1,9,8]]
+                [[],[[],8],[]])))
+  (is (= true (correct-packets?
+               [[[3,6,9],8,2]]
+               [[8,0,7],[7,[[8,8,1,4,5],9]],[9,9]])))
+  (is (= false (correct-packets?
+                [[[[2,3,9,10]],3,4,7]]
+                [[],[4,5,7,[4,10],[]]])))
+  (is (= false (correct-packets?
+                [[],[6]]
+                [[],[5]])))
+  (is (= false (correct-packets?
+                [[6] [[[9 8 7] 2 [2 1 7 7 8] [0 7 7]] 9 5 7]]
+                [[6]])))
+  (is (= true (correct-packets?
+               [[6]]
+               [[6] [[[9 8 7] 2 [2 1 7 7 8] [0 7 7]] 9 5 7]])))
+  (is (= :next (correct-packets?
+                [[6]]
+                [[6]])))
+  (is (= true (sorter
+               [[2]]
+               [[[2] [7] 6 9 0]])))
+  (is (= false (sorter
+                [[[2] [7] 6 9 0]]
+                [[2]]))))
+
 
 (deftest answer-a-test
-  (is (= (answer-a) "Not implemented yet")))
+  (is (= 13 (answer-a "data/problem-13-a.txt"))
+      (= 6478 (answer-a "data/problem-13-input.txt"))))
 
 (deftest answer-b-test
-  (is (= (answer-b) "Not implemented yet")))
+  (is (= 140 (answer-b "data/problem-13-a.txt"))
+      (= 21922 (answer-b "data/problem-13-input.txt"))))
