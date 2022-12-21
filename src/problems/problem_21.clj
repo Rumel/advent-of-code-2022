c7f7298a662c3d272f8b89b8b4257d8d3cf85949
Dalton Dick <daltondick2010@gmail.com>
2022-12-20 23:30:24 -0600

    Day 21 Part A

--- src/problems/problem_21.clj
+++ src/problems/problem_21.clj
@@ -1,8 +1,45 @@
-(ns problems.problem-21)
+(ns problems.problem-21
+  (:require [common.helpers :refer [input]]))
 
-(defn answer-a [] "Not implemented yet")
-(defn answer-b [] "Not implemented yet")
+(defn parse-line
+  [line]
+  (let [[fm fk f1 ff f2] (re-matches #"(....): (....) (\+|\-|\/|\*) (....)" line)
+        [_ nk n1] (re-matches #"(....): (\d+)" line)]
+    (if (not (nil? fm))
+      [fk {:f (case ff
+                "*" *
+                "/" /
+                "-" -
+                "+" +)
+           :n1 f1
+           :n2 f2}]
+      [nk {:f nil :n1 (read-string n1)}])))
+
+(defn parse-input
+  [file]
+  (->> file
+       input
+       (map parse-line)
+       (into {})))
+
+(defn dfs
+  [graph k]
+  (let [cur (graph k)]
+    ;; (println cur)
+    (if (nil? (cur :f))
+      (cur :n1)
+      ((cur :f) (dfs graph (cur :n1)) (dfs graph (cur :n2))))))
+
+(defn answer-a
+  [file]
+  (let [graph (-> file
+                  parse-input)]
+    (dfs graph "root")))
+
+(answer-a "data/problem-21-a.txt")
+
+(defn answer-b [file] "Not implemented yet")
 
 (defn answer []
-  (println "21: A:" (answer-a))
-  (println "21: B:" (answer-b)))
+  (println "21: A:" (answer-a "data/problem-21-input.txt"))
+  (println "21: B:" (answer-b "data/problem-21-input.txt")))
--- test/problems/problem_21_test.clj
+++ test/problems/problem_21_test.clj
@@ -1,9 +1,12 @@
 (ns problems.problem-21-test
   (:require [clojure.test :as test :refer [deftest is]]
-            [problems.problem-21 :refer [answer-a answer-b]]))
+            [problems.problem-21
+             :refer
+             [answer-a
+              answer-b]]))
 
 (deftest answer-a-test
-  (is (= (answer-a) "Not implemented yet")))
+  (is (= 152 (answer-a "data/problem-21-a.txt"))))
 
 (deftest answer-b-test
-  (is (= (answer-b) "Not implemented yet")))
+  (is (= "Not implemented yet" (answer-b "Not implemented yet"))))
